532ca346a1c68c63616b75d32bff29f0de290f2f
John A <kayak.stated_0b@icloud.com>
2024-01-13 16:56:56 +0000

    Dev Container cleanup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
 }
 
 //login user
