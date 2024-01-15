a08c9420bf4cf515591780519b1c1d7b2b9847da
John A <kayak.stated_0b@icloud.com>
2024-01-15 15:53:02 +0000

    Added AuthContext and Auth hook in Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
+import { AuthContext } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <WorkoutsContextProvider>
-        <App />
-    </WorkoutsContextProvider>
+  <React.StrictMode> 
+    <AuthContext>
+        <WorkoutsContextProvider>
+            <App />
+        </WorkoutsContextProvider>
+    </AuthContext>
   </React.StrictMode>
 );
 
