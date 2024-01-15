51a6d2ec3287efe32f7e96196de23254043b4a69
John A <kayak.stated_0b@icloud.com>
2024-01-15 16:17:40 +0000

    Added Signup and Login forms in Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
-import { AuthContext } from './context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-    <AuthContext>
-        <WorkoutsContextProvider>
-            <App />
-        </WorkoutsContextProvider>
-    </AuthContext>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
