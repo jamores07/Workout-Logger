6e4349ca482d5ba89573aa2fa04cbeddc0288044
John A <kayak.stated_0b@icloud.com>
2024-01-17 00:55:12 +0000

    Login/Logout functionality.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,7 +16,7 @@ const loginUser = async(req, res) => {
         //create a token 
         const token = createToken(user._id)
 
-        res.status(200).json({mssg: "Login successful"})
+        res.status(200).json({email, token})
     } catch(error){
         res.status(400).json({error: error.message})
     }
